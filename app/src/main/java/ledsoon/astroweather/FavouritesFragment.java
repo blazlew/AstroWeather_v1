fd206420e7698c308c1bcc78eccbc2b19fbc96fd
Blazej Lewandowski <ledek89@gmail.com>
2017-06-12 20:37:00 +0200

    Podlaczone pola fragmentu lokalizacji

--- app/src/main/java/ledsoon/astroweather/FavouritesFragment.java
+++ app/src/main/java/ledsoon/astroweather/FavouritesFragment.java
@@ -6,8 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FavouritesFragment extends Fragment implements SettingsFragment.IDataSender {
+
+    EditText etLocalization;
+    Button bPlus;
 
     @Override
     public void sendToActivity(String a, String b, String c) {
@@ -19,8 +24,10 @@ public class FavouritesFragment extends Fragment implements SettingsFragment.IDa
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favourites, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_favourites, container, false);
+        etLocalization = (EditText) rootView.findViewById(R.id.etLocalization);
+        bPlus = (Button) rootView.findViewById(R.id.bPlus);
+        return rootView;
     }
 
 }
